3b5309b5f8ce078e5a5fe978a4e71a689936d69b
Sarah Nosal <nosalsm@gmail.com>
2022-01-13 11:55:10 -0600

    commit tree dd2c46b5ddb8a0e6117f010ad6ca692271db2b92

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,25 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :albumm :id
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    # initialize with an id attribute cuz each row needs an id value for primary key
+    # set it to nil cuz it gets an id only when it is saved to database
+    @id = id
     @name = name
     @album = album
   end
 
+  # responsibility of class to create its own table
+  def self.create_table
+    # use <<- for strings that take up multiple lines- SQl means special word for beginning of doc
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
 end
